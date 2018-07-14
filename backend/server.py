2dfdcfc9f79b71e115a9394c1af5980c186910df
Rohan Kumar <rohan.carmel@gmail.com>
2018-07-14 10:59:42 +0530

    Flask app

--- backend/server.py
+++ backend/server.py
@@ -1,1 +1,11 @@
-print('hello world')
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/health-check', methods = ['GET', 'POST'])
+def healthCheck():
+    return "Success"
+
+if __name__ == '__main__':
+   app.run(host = '0.0.0.0')
